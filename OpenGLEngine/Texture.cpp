bc3c9d134885e44858da9f297c2a863f402afd21
Binh Nguyen Thanh <binhnguyenthanh@Binhs-MacBook-Pro-2.local>
2026-04-19 22:14:59 +0700

    Add SOIL:

--- OpenGLEngine/Texture.cpp
+++ OpenGLEngine/Texture.cpp
@@ -2,6 +2,9 @@
 // Created by Binh Nguyen Thanh on 19/4/26.
 //
 
+#include <SOIL/SOIL.h>
+#include <GL/glew.h>
+
 #include "Texture.h"
 
 Texture::Texture()
@@ -16,5 +19,18 @@ Texture::~Texture()
 }
 
 bool Texture::Load(const char *fileName) {
+    int channels = 0;
+    unsigned char* image = SOIL_load_image(
+        fileName,
+        &mWidth,
+        &mHeight,
+        &channels,
+        SOIL_LOAD_AUTO);
+    int format = GL_RGB;
+    if (channels == 4) {
+        format = GL_RGBA;
+    }
 
+    glGenTextures(1, &mTextureID);
+    glBindTexture(GL_TEXTURE_2D, mTextureID);
 }
